4d915bef7ef2491983c208b007223a93fdeb7490
Mithun <110370009+Mithunhubminepro@users.noreply.github.com>
2023-03-17 13:40:48 +0530

    Update 2.1.0

--- Guess_the_Number[1].py
+++ Guess_the_Number[1].py
@@ -1,37 +1,38 @@
-#This is the game's code
-#Update 1.1.1 Main Purpose of this update: Spelling Mistkes were Correct, another time.sleep and when thanking the user for playing we added the name varible there.
-import random
-import time
+import tkinter as tk
+import random as r
+import time as t
 
-Name = input ("Enter your name: ")
+#perparing!
+random_var = r.randint(1, 100)
+root = tk.Tk()
+root.title("Guess the Number by Mithun Creations")
+canvas = tk.Canvas(root, width=500, height=500)
+canvas.pack()
 
+ti = tk.Entry(root)
+ti.pack()
+var = ti.get()
 
+def lab ():
+    label = tk.Label(root, text="Hello! Welcome to random!", fg="blue", font=("Speak Pro", 12, "bold"))
+    label.pack()
+def lab2 ():
+    label2 = tk.Label (root, text=f"You Won! The answer is: {random_var}", fg = "green", font=("Speak Pro", 12, "bold")) 
+    label2.pack()
+def lab3 ():
+    label3 = tk.Label (root, text=f"You Lost! The answer is: {random_var}", fg = "red", font=("Speak Pro", 12, "bold"))
+    label3.pack()
+def check ():
+    root.update()
+    root.update_idletasks()
+    random_var = r.randint(1,100)
 
+    if var == random_var:
+        lab2()
+    if not var == random_var:
+        lab3()
+button = tk.Button(root, text="Check your number", command=check)        
+button.pack()
+lab()
 
-
-#perparing!!!
-random_var = random.randint(1, 100)
-sleep = random.randint(1,3)
-
-#Welcoming the user
-print ("Welcome to random guesser! You'll enter a number try to guess the number! If the number is wrong the system will say its worng and if you're correct then it'll say correct")
-
-#letting the user to guess!
-guess = int(input("Enter a number: "))
-
-#adding this "time.sleep" to trick the user that its loading! but its not!
-time.sleep (sleep)
-
-#if / if not
-if guess == random_var:
-    print("Correct! You Won!")
-
-if not guess == random_var:
-    print ("Wrong! You Lost!")
-    print(f"Answer is: {random_var}")
-
-#I guess nobody will win! Mabye one in one trillion?
-
-print (f"Thanks for playing {Name}!!!")
-
-time.sleep (5)
+root.mainloop()
